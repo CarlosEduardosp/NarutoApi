5ed56dd9e14cb1a8568a4c6546012b4bfd2b1196
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-04-28 01:32:56 -0300

    atualizando pytest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.infra.config import DBConnectionHandler, Base
 from src.infra.entities import users, Users
 
+
 db_conn = DBConnectionHandler()
 engine = db_conn.get_engine()
 Base.metadata.create_all(engine)
--- src/infra/repo/user_Repository.py
+++ src/infra/repo/user_Repository.py
@@ -1,8 +1,12 @@
 # pylint: disable=E1101
-
+from sqlalchemy import text
 from collections import namedtuple
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Users
+
+
+db_connection_handler = DBConnectionHandler()
+engine = db_connection_handler.get_engine()
 
 
 class UserRepository:
@@ -31,7 +35,6 @@ class UserRepository:
 
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
-
                 return new_user
             except:
                 db_connection.session.rollback()
@@ -39,3 +42,32 @@ class UserRepository:
             finally:
                 db_connection.session.close()
         return None
+
+    @classmethod
+    def select_user(self):
+        """Select in users"""
+        try:
+            with engine.connect() as connection:
+                # select data in users
+                query_user = connection.execute(text(f"SELECT * FROM users;"))
+
+                lista = []
+                for i in query_user:
+                    dados = {"nome": i.name, "resumo": i.resumo, "nivel": i.nivel, "vila": i.vila, "tecnicas": i.tecnicas, "url": i.url}
+                    lista.append(dados)
+
+                return lista
+        except:
+            return "erro"
+
+    @classmethod
+    def delete_user(cls, id: int):
+        """deleting data in users"""
+
+        try:
+            """ deleting data of select in users """
+            with engine.connect() as connection:
+                connection.execute(text(f"DELETE FROM users WHERE id={id} ;"))
+                connection.commit()
+        except:
+            return "erro ao deletar"
