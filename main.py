cb8288cb409939878e5d7cd8bbfa8c70b9c8901a
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-09-30 23:00:29 -0300

    feat: subindo para deploy

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from lerbanco import LerBanco, MontandoJson
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+
+# Configurar CORS (Cross-Origin Resource Sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Substitua pelo domínio do seu aplicativo
+    allow_credentials=True,
+    allow_methods=["GET"],  # Você pode especificar métodos permitidos, como ["GET", "POST"]
+    allow_headers=["Authorization"],  # Você pode especificar cabeçalhos permitidos, como ["Authorization"]
+)
 
 
 @app.get('/')
